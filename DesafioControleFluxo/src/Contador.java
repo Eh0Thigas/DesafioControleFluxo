7da3b7c3729c8cc56acf362f29e52ff68ffd927f
Thiago <thiagolaurenti123@gmail.com>
2022-08-29 16:09:00 -0300

    l

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Contador {
     
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite o parametro numero 1");
+        int p1 = s.nextInt();
+
+        System.out.println("Digite o parametro numero 2");
+        int p2 = s.nextInt();
+
+        try{
+            contar(p1,p2);
+        }catch(ParametrosInvalidosExeption e){
+
+        }
+    }
+
+    static void contar( int p1, int p2)throws ParametrosInvalidosExeption{
+
+    }
+
 }
